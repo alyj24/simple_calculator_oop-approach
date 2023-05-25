07752249f2bbb494ec259b1eca98f8d653dd385b
Family <albon.136546120855@2depedqc.ph>
2023-05-25 20:22:48 +0800

    First Function

--- user_module.py
+++ user_module.py
@@ -1,4 +1,10 @@
-# psedocode
+class UserInterface:
+# pseudocode
 # ask the user for its choice of operation
+
+def basic_math_operations(self):
+    print("I have here the basic math operations which are the Addition, Subtraction, Multiplication, and Division")
+    selection = input("Pick a basic math operation among these four (+|-|*|/): ")
+    return selection
 # ask the user for two variables to solve
 # once again, ask the user if want to try again or not
