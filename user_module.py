a7214e0985e113c6a8f4b942c8e94ebf3b8cde0c
Family <albon.136546120855@2depedqc.ph>
2023-05-25 20:25:16 +0800

    Asking for two numbers in

--- user_module.py
+++ user_module.py
@@ -1,10 +1,17 @@
 class UserInterface:
 # pseudocode
 # ask the user for its choice of operation
-
 def basic_math_operations(self):
     print("I have here the basic math operations which are the Addition, Subtraction, Multiplication, and Division")
     selection = input("Pick a basic math operation among these four (+|-|*|/): ")
     return selection
+
 # ask the user for two variables to solve
+def var1(self):
+    var1 = float(input("Enter your first variable number: "))
+    return var1
+
+def var2(self):
+    var2 = float(input("Enter your second variable number: "))
+    return var2
 # once again, ask the user if want to try again or not
