30df864b361a65f0443b0c021091718f306bf223
Family <albon.136546120855@2depedqc.ph>
2023-05-25 20:33:09 +0800

    Execution of Operations are coded

--- calculator_app.py
+++ calculator_app.py
@@ -3,6 +3,21 @@ class SimpleAppCalculator:
     # recognize the two input numbers and solve for  the choice of operation
     # define the operations
     # addition
+    def __add__(self, var1, var2):
+        sum = var1 + var2
+        return sum
+
     # subtraction
+    def __subtract__(self, var1, var2):
+        difference = var1 - var2
+        return difference
+
     # multiplication
+    def __multiply__(self, var1, var2):
+        product = var1 * var2
+        return product
+
     # division
+    def __divide__(self, var1, var2):
+        quotient = var1 / var2
+        return quotient
