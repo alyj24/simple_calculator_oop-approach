c8bb6bb425aeba39940815fb49f2903d9e153bca
Family <albon.136546120855@2depedqc.ph>
2023-05-25 21:24:34 +0800

    Finally working

--- calculator_oop.py
+++ calculator_oop.py
@@ -27,29 +27,34 @@ def simple_app_calculator():
         var1 = interaction.var1()
         var2 = interaction.var2()
     # execute the recognized chosen operation
-    if selection == "+":
-        sum = program.__add__(var1, var2)
-        interaction.sum(sum)
-    elif selection == "-":
-        difference = program.__subtract__(var1, var2)
-        interaction.difference(difference)
-    elif selection == "*":
-        product = program.__multiply__(var1, var2)
-        interaction.product(product)
-    elif selection == "/":
-        quotient = program.__divide__(var1, var2)
-        interaction.quotient(quotient)
-    else:
-        print("Oh no! There must be a problem in your input to be invalid.")
+        if selection == "+":
+           sum = program.__add__(var1, var2)
+           interaction.sum(sum)
 
-    while True:
-        option = interaction.retry()
-        if option == "yes, indeed":
-            return simple_app_calculator()
-        elif option == "already satisfied"
-            print(("Thank you so much!")
+        elif selection == "-":
+           difference = program.__subtract__(var1, var2)
+           interaction.difference(difference)
+
+        elif selection == "*":
+            product = program.__multiply__(var1, var2)
+            interaction.product(product)
+
+        elif selection == "/":
+            quotient = program.__divide__(var1, var2)
+            interaction.quotient(quotient)
         else:
-            exit()
+             print("Oh no! There must be a problem in your input to be invalid.")
+
+        while True:
+             option = interaction.retry()
+             if option == "yes, indeed":
+                return simple_app_calculator()
+
+             elif option == "already satisfied":
+                   print("Thank you so much!")
+
+             else:
+                   exit()
 
     except ValueError:
         print("Oops! Take a look back for a second, there must be a problem.")
--- user_module.py
+++ user_module.py
@@ -1,35 +1,35 @@
 class UserInterface:
 # pseudocode
 # ask the user for its choice of operation
-def basic_math_operations(self):
-    print("I have here the basic math operations which are the Addition, Subtraction, Multiplication, and Division")
-    selection = input("Pick a basic math operation among these four (+|-|*|/): ")
-    return selection
+    def basic_math_operations(self):
+        print("I have here the basic math operations which are the Addition, Subtraction, Multiplication, and Division")
+        selection = input("Pick a basic math operation among these four (+|-|*|/): ")
+        return selection
 
 # ask the user for two variables to solve
-def var1(self):
-    var1 = float(input("Enter your first variable number: "))
-    return var1
+    def var1(self):
+        var1 = float(input("Enter your first variable number: "))
+        return var1
 
-def var2(self):
-    var2 = float(input("Enter your second variable number: "))
-    return var2
+    def var2(self):
+        var2 = float(input("Enter your second variable number: "))
+        return var2
 
 # print the result of the inputs
-'''addition'''
-def sum(self, sum):
-    print("The sum of your input is: ", sum)
-'''subtraction'''
-def difference(self, difference):
-    print("The difference of your input is: ", difference)
-'''multiplication'''
-def product(self, product):
-    print("The product of your input is: ", product)
-'''division'''
-def quotient(self, quotient):
-    print("The quotient of your input is: ", quotient)
+    '''addition'''
+    def sum(self, sum):
+        print("The sum of your input is: ", sum)
+    '''subtraction'''
+    def difference(self, difference):
+        print("The difference of your input is: ", difference)
+    '''multiplication'''
+    def product(self, product):
+        print("The product of your input is: ", product)
+    '''division'''
+    def quotient(self, quotient):
+        print("The quotient of your input is: ", quotient)
 
 # once again, ask the user if want to try again or not
-def retry(self):
-    option = input("Nice choice of your operation and numbers! Do you want to try again? (yes, indeed/already satisfied): ")
-    return option
+    def retry (self):
+        option = input("Nice choice of your operation and numbers! Do you want to try again? (yes, indeed/already satisfied): ")
+        return option
