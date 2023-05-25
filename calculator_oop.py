e2c2e74b1bb2965e28f5c0c760ad1039f1f467ef
Family <albon.136546120855@2depedqc.ph>
2023-05-25 20:12:45 +0800

    Instruction & Pseudocode has been written

--- calculator_oop.py
+++ calculator_oop.py
@@ -2,3 +2,9 @@ print("Assignment 7".center(45, "="))
 print("~" * 45)
 print("CMPE-103-MODULE-4-APPLYING-THE CONCEPT-OF-OOP")
 print("~" * 45)
+
+# redo the creation of a simple app calculator program with the concept of OOP.
+
+# pseudocode
+# create the class of user interface
+# create another class for the calculator
