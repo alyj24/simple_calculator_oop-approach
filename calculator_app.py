10b357c2f29faeddc174d7e83af72e0d89f7afb7
Family <albon.136546120855@2depedqc.ph>
2023-05-25 20:28:34 +0800

    Class for the Simple Calculator Done

---
+++ calculator_app.py
@@ -0,0 +1,8 @@
+class SimpleAppCalculator:
+    # pseudocode
+    # recognize the two input numbers and solve for  the choice of operation
+    # define the operations
+    # addition
+    # subtraction
+    # multiplication
+    # division
